ee81ff3bb353706a991ed16435c95fb130fd3b76
ChazArvizu <cryan018@gmail.com>
2022-10-27 20:53:35 -0700

    added useful folders

---
+++ Hexadecimators.BreazyFit/Hexadecimators.BreazyFit.Logging/Implementations/DatabaseLogger.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hexadecimators.BreazyFit.Logging.Implementations
+{
+    public class DatabaseLogger
+    {
+    }
+}
