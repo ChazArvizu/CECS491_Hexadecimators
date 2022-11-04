e8dac77c7c22a863adbbd44a6a334c973b082df1
MadKid1000 <adelarosa1000@gmail.com>
2022-11-03 21:00:38 -0700

    update gitignore

--- Hexadecimators.BreazyFit/Hexadecimators.BreazyFit.Client/Program.cs
+++ Hexadecimators.BreazyFit/Hexadecimators.BreazyFit.Client/Program.cs
@@ -3,7 +3,7 @@
     private static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
-        Console.WriteLine("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+
         Console.ReadLine();
     }
 }
