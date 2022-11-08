e6f4e8a1bf8682e1451f079847f99309edfa22a9
ChazArvizu <cryan018@gmail.com>
2022-11-07 21:54:49 -0800

    Adding testing

---
+++ Hexadecimators.BreazyFit/Hexadecimators.BreazyFit.Logging.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Hexadecimators.BreazyFit.Logging.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
