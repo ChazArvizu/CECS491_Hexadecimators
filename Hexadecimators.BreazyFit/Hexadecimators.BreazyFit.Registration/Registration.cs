d35ba15da42592614603989c6472775296a8a14b
sean9731 <sean.iida159@gmail.com>
2022-11-08 16:48:57 -0800

    consolidate registration code

--- Hexadecimators.BreazyFit/Hexadecimators.BreazyFit.Registration/Registration.cs
+++ Hexadecimators.BreazyFit/Hexadecimators.BreazyFit.Registration/Registration.cs
@@ -1,3 +1,4 @@
+﻿
 ﻿using Microsoft.Data.SqlClient;
 using Microsoft.SqlServer;
 using Microsoft.Identity.Client;
@@ -17,12 +18,16 @@ namespace Hexadecimators.BreazyFit.Registration
         static string connectionString = @"Server=.\;Database=Hexadecimators.BreazyFit.Users;Integrated Security=True;Encrypt=False";
 
 
-        public static Boolean emailValidate(string email, SqlConnection conn)
+        public static Boolean inputValidate(string email, string pass, SqlConnection conn)
         {
             /* REQUIREMENTS: 
+            EMAIL:
            -must be in valid email format
            -database cannot have the inputted email already
 
+            PASSWORD:
+            -must be minimum 8 chars
+            -must use valid chars
             */
 
 
@@ -31,12 +36,51 @@ namespace Hexadecimators.BreazyFit.Registration
 
             string emailQuery = "SELECT [Email] FROM [Hexadecimators.BreazyFit.Users].[dbo].[Users] WHERE [Email]='" + email + "';";
 
+
             var command = new SqlCommand(emailQuery, conn);
             command.ExecuteNonQuery();
 
             //create a reader object to return the rows selected
 
             SqlDataReader reader = command.ExecuteReader();
+
+            //**Email and User Validation**
+
+            string nameChunk;
+            string addressChunk;
+
+            //check that @ char is present only once
+
+            if (email.IndexOf('@') > 0 && email.Count(c => c == '@') == 1)
+            {
+
+                nameChunk = email.Substring(0, email.IndexOf('@'));
+                addressChunk = email.Substring(email.IndexOf('@') + 1);
+
+
+            }
+            else
+            {
+                nameChunk = "";
+                addressChunk = "";
+            }
+
+
+            //if email is not of valid formatting, do not create account
+
+            if (nameChunk.Length <= 0 || addressChunk.Length <= 0 || addressChunk.EndsWith('.') || addressChunk.IndexOf('.') == -1)
+            {
+
+                Console.WriteLine("Invalid Email format.");
+                reader.Close();
+                return false;
+
+            }
+
+            //   sean@gmail.co.us
+
+            //if SELECT query returns any rows, do not create account due to taken username
+
             if (reader.HasRows)
             {
                 //if SELECT query returns any rows, do not create account due to taken username
@@ -51,16 +95,6 @@ namespace Hexadecimators.BreazyFit.Registration
             return true;
         }
 
-
-        public static Boolean passwordValidate(string password, SqlConnection conn)
-        {
-            /* REQUIREMENTS: 
-                   -8 characters minimum
-                   - 
-
-            */
-            return true;
-        }
 
 
 
@@ -88,16 +122,16 @@ namespace Hexadecimators.BreazyFit.Registration
 
                 //validate input before executing sql
 
-                if (emailValidate(email, connection) == true && passwordValidate(password, connection) == true)
-                {
+                    if (inputValidate(email, password, connection) == true)
+                    {
 
-                    //pass our query string to the connection string
+                        //pass our query string to the connection string
 
-                    var command = new SqlDataAdapter(userQuery, connection);
-                    command.SelectCommand.ExecuteNonQuery();
+                        var command = new SqlDataAdapter(userQuery, connection);
+                        command.SelectCommand.ExecuteNonQuery();
 
-                    Console.WriteLine("User: " + userUsername + "\n Account created successfully.");
-                }
+                        Console.WriteLine("User: " + userUsername + "\n Account created successfully.");
+                    }
 
 
 
